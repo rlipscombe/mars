b1bf1703d1c8890f4003ba7df0808c2024c8f334
Roger Lipscombe <roger@differentpla.net>
2022-09-07 22:50:23 +0100

    Test movement when scented

--- mars/test/scent_test.dart
+++ mars/test/scent_test.dart
@@ -14,4 +14,24 @@ void main() {
     robot.run(grid);
     assert(grid.isMarked(Position(3, 3)));
   });
+
+  test('robots should not move off the world if scented', () {
+    var grid = Grid(1, 1)..mark(Position(1, 1));
+    var robot = Robot(1, 1, Direction.north)
+      ..instructions = [Instruction.forward];
+    robot.run(grid);
+    expect(robot.position.x, 1);
+    expect(robot.position.y, 1);
+    expect(robot.isLost, false);
+  });
+
+  test('robots can move forward on scented squares if safe', () {
+    var grid = Grid(1, 1)..mark(Position(1, 1));
+    var robot = Robot(1, 1, Direction.west)
+      ..instructions = [Instruction.forward];
+    robot.run(grid);
+    expect(robot.position.x, 0);
+    expect(robot.position.y, 1);
+    expect(robot.isLost, false);
+  });
 }
