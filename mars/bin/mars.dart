e6911e8073887fdd4e8721ffb903ee823df7529a
Roger Lipscombe <roger@differentpla.net>
2022-09-07 20:40:53 +0100

    Initial creation

---
+++ mars/bin/mars.dart
@@ -0,0 +1,5 @@
+import 'package:mars/mars.dart' as mars;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mars.calculate()}!');
+}
---
+++ mars/test/mars_test.dart
@@ -0,0 +1,8 @@
+import 'package:mars/mars.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
