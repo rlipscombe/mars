5aae800c75b9e2746ea0cfe74519faac1b4be79d
Roger Lipscombe <roger@differentpla.net>
2022-09-08 19:38:39 +0100

    Implement rest of grammar

--- redplanet/src/main.rs
+++ redplanet/src/main.rs
@@ -4,11 +4,29 @@ use std::fs;
 mod model;
 use crate::model::*;
 
-fn parser() -> impl Parser<char, Grid, Error = Simple<char>> {
+fn parser() -> impl Parser<char, Problem, Error = Simple<char>> {
     let num = text::int(10).map(|s: String| s.parse().unwrap()).padded();
 
     let grid = num.then(num).map(|(x, y)| Grid { x, y });
-    grid
+    let direction = choice((
+        just("N").to(Direction::North),
+        just("E").to(Direction::East),
+        just("S").to(Direction::South),
+        just("W").to(Direction::West),
+    )).padded();
+    let instruction = choice((
+        just("L").to(Instruction::Left),
+        just("R").to(Instruction::Right),
+        just("F").to(Instruction::Forward),
+    ));
+    let robot = num
+        .then(num)
+        .then(direction)
+        .then(instruction.repeated().padded())
+        .map(|(((x, y), d), instructions)| Robot { x, y, direction: d, instructions });
+    let robots = robot.repeated();
+    grid.then(robots)
+        .map(|(grid, robots)| Problem { grid, robots })
 }
 
 fn main() {
--- redplanet/src/model.rs
+++ redplanet/src/model.rs
@@ -1,5 +1,34 @@
+#[derive(Debug, Clone)]
+pub enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
+
+#[derive(Debug, Clone)]
+pub enum Instruction {
+    Left,
+    Right,
+    Forward,
+}
+
 #[derive(Debug)]
 pub struct Grid {
     pub x: i32,
     pub y: i32,
+}
+
+#[derive(Debug)]
+pub struct Robot {
+    pub x: i32,
+    pub y: i32,
+    pub direction: Direction,
+    pub instructions: Vec<Instruction>,
+}
+
+#[derive(Debug)]
+pub struct Problem {
+    pub grid: Grid,
+    pub robots: Vec<Robot>,
 }
