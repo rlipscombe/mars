3effba99ee4bc2b886bb8c52f1779c61d3712561
Roger Lipscombe <roger@differentpla.net>
2022-09-08 16:43:35 +0100

    Read the file by lines

--- redplanet/src/main.rs
+++ redplanet/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::fs;
+
+fn main() -> Result<(), Box<dyn Error>>{
+    let input = fs::read_to_string("input.txt")?;
+    for line in input.lines() {
+        println!("{}", line);
+    }
+    Ok(())
 }
