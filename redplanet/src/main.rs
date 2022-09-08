cff531cb9c08a5069f52883fdd287faa63b5715d
Roger Lipscombe <roger@differentpla.net>
2022-09-08 19:42:03 +0100

    Move parser to separate file

--- redplanet/src/main.rs
+++ redplanet/src/main.rs
@@ -1,33 +1,10 @@
-use chumsky::prelude::*;
 use std::fs;
+use chumsky::prelude::*;
 
+mod parser;
 mod model;
-use crate::model::*;
 
-fn parser() -> impl Parser<char, Problem, Error = Simple<char>> {
-    let num = text::int(10).map(|s: String| s.parse().unwrap()).padded();
-
-    let grid = num.then(num).map(|(x, y)| Grid { x, y });
-    let direction = choice((
-        just("N").to(Direction::North),
-        just("E").to(Direction::East),
-        just("S").to(Direction::South),
-        just("W").to(Direction::West),
-    )).padded();
-    let instruction = choice((
-        just("L").to(Instruction::Left),
-        just("R").to(Instruction::Right),
-        just("F").to(Instruction::Forward),
-    ));
-    let robot = num
-        .then(num)
-        .then(direction)
-        .then(instruction.repeated().padded())
-        .map(|(((x, y), d), instructions)| Robot { x, y, direction: d, instructions });
-    let robots = robot.repeated();
-    grid.then(robots)
-        .map(|(grid, robots)| Problem { grid, robots })
-}
+use parser::*;
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("read input file");
